e63218f306ed3b38071012903581cc77dcaae5da
Joshua Lyngdoh <joshualyngdoh52@gmail.com>
2023-02-20 19:03:21 +0530

    Add routes and styles

---
+++ src/CalculatorPg.js
@@ -0,0 +1,23 @@
+/* eslint-disable react/no-unescaped-entities */
+
+import React from 'react';
+import './index.css';
+import Calculator from './components/calculator';
+
+function Calculate() {
+  return (
+    <div>
+      <h1>Math Magician</h1>
+      <div className="cal-body">
+        <div>
+          <h2 className="cal-bodyh2">Let's do some math!</h2>
+        </div>
+        <div className="calculator-body">
+          <Calculator />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Calculate;
