23de679d05d54d1c455bb35935b9ab3763024966
EBrain <ebrain.ebrain@gmail.com>
2023-02-21 19:41:48 +0630

    Update App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 import Nav from './components/Nav';
 import Home from './pages/Home';
 import Quote from './pages/Quotes';
