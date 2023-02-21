1f22ce6d1903789c4143c5e6848dd360dcde724d
Joshua Lyngdoh <joshualyngdoh52@gmail.com>
2023-02-21 17:33:34 +0530

    Changes to src directory

---
+++ src/logic/calculate.test.js
@@ -0,0 +1,40 @@
+import calculate from './calculate';
+
+describe('testing for calculate funtion', () => {
+  const num = {
+    total: null,
+    next: null,
+    operation: null,
+  };
+  test('for multiplication', () => {
+    let result = calculate(num, '10');
+    result = calculate(result, 'x');
+    result = calculate(result, '8');
+    result = calculate(result, '=');
+    expect(result.total).toBe('80');
+  });
+
+  test('for addition', () => {
+    let result = calculate(num, '10');
+    result = calculate(result, '+');
+    result = calculate(result, '8');
+    result = calculate(result, '=');
+    expect(result.total).toBe('18');
+  });
+
+  test('for subtraction', () => {
+    let result = calculate(num, '24');
+    result = calculate(result, '-');
+    result = calculate(result, '6');
+    result = calculate(result, '=');
+    expect(result.total).toBe('18');
+  });
+
+  test('for division', () => {
+    let result = calculate(num, '8');
+    result = calculate(result, '÷');
+    result = calculate(result, '2');
+    result = calculate(result, '=');
+    expect(result.total).toBe('4');
+  });
+});
